6c8adf6c28f7f2d3aac649b49f611e67a7797e0f
Alex <traderalexngcy59@gmail.com>
2022-07-18 10:00:01 +0800

    feat: update build dependencies + execute script

--- scripts/build-dependencies.sh
+++ scripts/build-dependencies.sh
@@ -1,20 +1,26 @@
+# Note: update to versions came from https://github.com/google/libphonenumber/pull/2702
+
 rm -rf libphonenumber
 git clone --branch v`cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['googleLibphonenumberVersion'])"` https://github.com/googlei18n/libphonenumber/
 
 if [ ! -d ./closure-library/.git ]; then
     rm -rf closure-library
-    git clone --branch v20180910 https://github.com/google/closure-library/
+    git clone --branch v20201006 https://github.com/google/closure-library/
 fi
 
-if [ ! -f ./closure-compiler/target/closure-compiler-1.0-SNAPSHOT.jar ]; then
-    rm -rf closure-compiler
-    git clone --branch v20181210 https://github.com/google/closure-compiler.git
+if [ ! -f ./closure-compiler/bazel-bin/compiler_unshaded_deploy.jar ]; then
+    git clone https://github.com/google/closure-compiler.git
     cd closure-compiler
-    mvn -DskipTests -pl externs/pom.xml,pom-main.xml,pom-main-shaded.xml
+    # yarn build
+    bazelisk build //:compiler_unshaded_deploy.jar
     cd ..
 fi
 
 if [ ! -d ./closure-linter/.git ]; then
     rm -rf closure-linter
     git clone --branch v2.3.19 https://github.com/google/closure-linter.git
+fi
+
+if [ ! -d ./python-gflags/.git ]; then
+    git clone -b 3.1.2 https://github.com/google/python-gflags.git
 fi
--- scripts/execute.sh
+++ scripts/execute.sh
@@ -8,6 +8,7 @@ cp src/PhoneNumberMetadata.xml libphonenumber/resources/PhoneNumberMetadata.xml
 cp src/PhoneNumberMetadataForTesting.xml libphonenumber/resources/PhoneNumberMetadataForTesting.xml
 
 ant -f libphonenumber/java/build.xml build-js-metadata
+ant -f libphonenumber/javascript/build.xml compile-demo
 
 mkdir dist
 node src/minify.js
