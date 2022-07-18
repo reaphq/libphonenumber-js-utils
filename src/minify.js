e1c3e7c22829f9300af6d2e1dcb4902d0c597fc4
Alex <traderalexngcy59@gmail.com>
2022-07-18 09:59:01 +0800

    feat: use gcc when compiling distribution file

--- src/minify.js
+++ src/minify.js
@@ -1,10 +1,13 @@
-var path = require('path');
-var compressor = require('node-minify');
+const path = require('path');
+const minify = require('@node-minify/core');
+const gcc = require('@node-minify/google-closure-compiler');
 
-(function () {
-  compressor.minify({
-    compressor: 'uglifyjs',
-    input: './libphonenumber/javascript/i18n/phonenumbers/demo-compiled.js',
-    output: path.resolve(__dirname, '../dist/libphonenumber.js'),
-  });
-})();
+// Using Google Closure Compiler
+minify({
+  compressor: gcc,
+  input: './libphonenumber/javascript/i18n/phonenumbers/demo-compiled.js',
+  output: path.resolve(__dirname, '../dist/libphonenumber.js'),
+  callback: function(err, min) {
+    console.log(err)
+  }
+});
